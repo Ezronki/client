72b81c4c5c0c0ee60f260b3b16f6216199a6920d
Ezronki <ezronkiprotich@gmail.com>
2025-03-24 11:47:19 +0300

    server

--- src/components/admin-view/image-upload.jsx
+++ src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "https://server-017e.onrender.com/api/admin/products/upload-image",
+      "https://server-e44q.onrender.com/api/admin/products/upload-image",
       data
     );
     console.log(response, "response");
